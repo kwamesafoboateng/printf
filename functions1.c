a5526004f5f99ff62f24889c388bc7ca48c54051
WNota <133232184+WNota@users.noreply.github.com>
2023-08-20 21:57:32 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -3,9 +3,9 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
+ * print_unsigned - Prints an unsigned number.
  * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * @buffer: Buffer array to handle print.
  * @flags:  Calculates active flags
  * @width: get width
  * @precision: Precision specification
